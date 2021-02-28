304cfd8e5e0ceb05e75988f066d4845a7a53b0a0
feddot2517@gmail.com <8D2g1101>
2021-02-28 12:36:07 +0600

    qwe

--- js/render.js
+++ js/render.js
@@ -1,131 +1,194 @@
 const item = [
 
     {
-        videoPosition: 'right',
+        videoPosition: 'left',
         type: 'masks',
-        title: 'Ball Game!',
+        title: 'Yolo Shop',
         body:
             `Маска для Московского онлайн магазина Yolo Shop.
             Эффект содержит неоновую рамку с названием бренда.
             При моргании появляются зеленые линзы.`,
-        video: 'mask1.mp4',
-        fontColor: 'white',
-        streak: 'white',
+        video: 'mask1.MOV',
+        fontColor: 'black',
+        streak: 'blue',
         background: '#ffffff',
     },
 
     {
-        videoPosition: 'left',
-        type: 'masks',
-        title: 'Ball Game!',
-        body:
-            `Маска для Московского онлайн магазина Yolo Shop.
-            Эффект содержит неоновую рамку с названием бренда.
-            При моргании появляются зеленые линзы.`,
-        video: 'mask1.mp4',
-        fontColor: 'black',
-        streak: 'white',
-        background: '#68afe0'
-    },
-    {
         videoPosition: 'right',
         type: 'masks',
-        title: 'Ball Game!',
+        title: 'Танос',
         body:
-            `Маска для Московского онлайн магазина Yolo Shop.
-            Эффект содержит неоновую рамку с названием бренда.
-            При моргании появляются зеленые линзы.`,
-        video: 'mask1.mp4',
-        fontColor: 'black',
-        streak: 'white',
-        background: '#ffffff'
-    },
-
-    {
-        videoPosition: 'left',
-        type: 'masks',
-        title: 'Ball Game!',
-        body:
-            `Маска для Московского онлайн магазина Yolo Shop.
-            Эффект содержит неоновую рамку с названием бренда.
-            При моргании появляются зеленые линзы.`,
-        video: 'mask1.mp4',
-        fontColor: 'black',
-        streak: 'white',
-        background: '#68afe0'
-    },
-    {
-        videoPosition: 'right',
-        type: 'games',
-        title: 'Игрулечька!',
-        body:
-            `Прикольная игра игрулечка коля сделал вщ класс!!!!`,
-        video: 'mask1.mp4',
-        fontColor: 'black',
-        streak: 'white',
-        background: '#ffffff'
-    },
-
-    {
-        videoPosition: 'left',
-        type: 'games',
-        title: 'Еще одна игрулечка!!',
-        body:
-            `Тоже коля делал!!! ВЩ ОЧЕНЬ КРУТАЯ!!!`,
-        video: 'mask1.mp4',
-        fontColor: 'black',
-        streak: 'white',
-        background: '#a470fd'
-    },
-    {
-        videoPosition: 'right',
-        type: 'games',
-        title: 'Игрулечька!',
-        body:
-            `Прикольная игра игрулечка коля сделал вщ класс!!!!`,
-        video: 'mask1.mp4',
-        fontColor: 'black',
-        streak: 'white',
-        background: '#ffffff'
-    },
-
-    {
-        videoPosition: 'left',
-        type: 'games',
-        title: 'Еще одна игрулечка!!',
-        body:
-            `Тоже коля делал!!! ВЩ ОЧЕНЬ КРУТАЯ!!!`,
-        video: 'mask1.mp4',
+            `Маска с главным злодеем известного фильма "Мстители". Персонаж повторяет действия за лицом человека. При записи происходит замена голоса.`,
+        video: 'mask2.MOV',
         fontColor: 'white',
         streak: 'white',
-        background: '#a470fd'
+        background: 'linear-gradient(to right, #006DF0, #007CF8 )',
+
     },
+    {
+        videoPosition: 'left',
+        type: 'masks',
+        title: 'Хам Миллион',
+        body:
+            `Маска-рандом для Омского Бара Хам-Миллион. Над головой человека крутится надпись с названием заведения.
+             В неоновой рамке случайным образом выпадает напиток, на который будет действовать скидка. На лице присутствует эффект светомузыки.`,
+        video: 'mask3.mp4',
+        fontColor: 'black',
+        streak: 'blue',
+        background: '#ffffff'
+    },
+
+    {
+        videoPosition: 'right',
+        type: 'masks',
+        title: 'Макияж',
+        body:
+            `Маска с 3D ресницами и тенями для блогера Екатерины Колисниченко. Эффект увеличивает губы, глаза и уменьшает нос.`,
+        video: 'mask4.mp4',
+        fontColor: 'white',
+        streak: 'white',
+        background: 'linear-gradient(to right, #006DF0, #007CF8 )'
+
+    },
+    {
+        videoPosition: 'left',
+        type: 'masks',
+        title: 'Респиратор',
+        body:
+            `Маска с 3D моделью респиратора и зелеными линзами. При открытии рта появляется зеленый дым`,
+        video: 'mask5.MOV',
+        fontColor: 'black',
+        streak: 'blue',
+        background: '#ffffff'
+    },
+
+    {
+        videoPosition: 'right',
+        type: 'masks',
+        title: 'Моргенштерн',
+        body:
+            `Маска с эффектом трекинга лица человека из видео.`,
+        video: 'mask6.MOV',
+        fontColor: 'white',
+        streak: 'white',
+        background: 'linear-gradient(to right, #006DF0, #007CF8 )'
+
+    },
+    {
+        videoPosition: 'left',
+        type: 'masks',
+        title: 'Тест на Вирус',
+        body:
+            `Маска с возможностью отвечать на вопросы. При наклоне головы выбирается ответ: да/нет, в зависимости от выбора определяется итог опроса.`,
+        video: 'mask7.mp4',
+        fontColor: 'black',
+        streak: 'blue',
+        background: '#ffffff'
+    },
+
     {
         videoPosition: 'right',
         type: 'games',
-        title: 'Игрулечька!',
+        title: 'Попробуй удержать',
         body:
-            `Прикольная игра игрулечка коля сделал вщ класс!!!!`,
-        video: 'mask1.mp4',
-        fontColor: 'black',
+            `Игра для футбольной группы footbline. Цель игры удержать мяч, балансируя им на голове. 
+            Если объект падает, то происходит перезапуск по касанию экрана.`,
+        video: 'game1.mp4',
+        fontColor: 'white',
         streak: 'white',
+        background: 'linear-gradient(to right, #006DF0, #007CF8 )'
+
+    },
+    {
+        videoPosition: 'left',
+        type: 'games',
+        title: 'Гонки',
+        body:
+            `Игра, где нужно управлять автомобилем для того, чтобы объехать другие машины. Управление осуществляется путем наклона головой. `,
+        video: 'game2.MOV',
+        fontColor: 'black',
+        streak: 'blue',
         background: '#ffffff'
+    },
+
+    {
+        videoPosition: 'right',
+        type: 'games',
+        title: 'Марио',
+        body:
+            `Игра, оформленная в стиле известной игры Марио. 
+            Управление персонажем осуществляется путем моргания.
+             Игра заканчивается, если врезаться в препятствие.`,
+        video: 'game3.MOV',
+        fontColor: 'white',
+        streak: 'white',
+        background: 'linear-gradient(to right, #006DF0, #007CF8 )'
+
     },
 
     {
         videoPosition: 'left',
         type: 'games',
-        title: 'Еще одна игрулечка!!',
+        title: 'ОмГТУ',
         body:
-            `Тоже коля делал!!! ВЩ ОЧЕНЬ КРУТАЯ!!!`,
-        video: 'mask1.mp4',
+            `Игра для Омского Государственного Технического Университета. 
+            С помощью наклонов головы можно управлять автомобилем и объезжать препятствия.`,
+        video: 'game4.MOV',
         fontColor: 'black',
+        streak: 'blue',
+        background: '#ffffff',
+        
+
+    },
+
+    
+    {
+        videoPosition: 'right',
+        type: 'games',
+        title: 'Поймай еду',
+        body:
+            `Игра для Омского кинотеатра "Галактика". Необходимо ловить ртом правильные предметы, в обратном случае будет отниматься одна жизнь. 
+            Игра заканчивается когда человек поймал 3 отрицательных объекта. Ведётся счет лучшего рекорда. `,
+        video: 'game5.MOV',
+        fontColor: 'white',
         streak: 'white',
-        background: '#a470fd'
+        background: 'linear-gradient(to right, #006DF0, #007CF8 )'
+        
+
+    },
+
+    {
+        videoPosition: 'left',
+        type: 'presets',
+        title: 'Teal & Orange',
+        body:
+            `Пресет, в котором преимущественно используются бирюзовый и оранжевый цвет. 
+            По касанию экрана появляются пылинки, а также текущий день недели и время.`,
+        video: 'preset1.MOV',
+        fontColor: 'white',
+        streak: 'white',
+        background: 'linear-gradient(to right, #006DF0, #007CF8 )'
+
+    },
+
+    {
+        videoPosition: 'left',
+        type: 'presets',
+        title: 'Kira Kira',
+        body:
+            `Пресет, выполненный в фиолетовых тонах. 
+            Эффект cодержит блестки, которые появляются на белом, а также ретушь, которая работает на двух человек одновременно.`,
+        video: 'preset2.mp4',
+        fontColor: 'white',
+        streak: 'white',
+        background: 'linear-gradient(to right, #006DF0, #007CF8 )'
+
     },
 ]
 
 document.body.onload = () => {
+    const nav = document.getElementById('navigator');
     item.forEach((item,index) => {
         const maskExample = document.createElement("div");
 
@@ -134,11 +197,11 @@ document.body.onload = () => {
                 <div class="container">
                     <div class="video ${item.videoPosition}">
                         <video id="player${index}" width="420" height="730" controls data-plyr-config='{"controls": ["play", "restart"]}' style="width: 348px; border-radius: 40px">
-                            <source src="media/mask1.mp4" type="video/mp4">
+                         <source src="media/${item.video}">
                         </video>
                         <div class="mask__example-description">
                             <div class="mask__example-description-title ${item.streak==='white'?'whitestreak':''}">
-                                ${item.type==="masks"?'Маска':item.type==="games"?"Игра":item.type="presets"?'Пресет':'Модель'}<br>Ball Game
+                                ${item.type==="masks"?'Маска':item.type==="games"?"Игра":item.type==="presets"?'Пресет':'Модель'}<br>${item.title}
                             </div>
                             <div class="mask__example-description-body" style="color:${item.fontColor};">
                                 ${item.body}
